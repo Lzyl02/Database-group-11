9fbc3ab944ced1af0c12d1cf545e822a974b9ae0
Yang <114745284+Yang8823@users.noreply.github.com>
2023-04-20 10:37:10 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -20,16 +20,38 @@
                       alert('Password do not match');
                  </script>";
         }
-
-        $sql = "SELECT * FROM `staffaccounts` WHERE `staff_ID` = '$staff_id' AND `password` = '$password'";
+        
+        // check if staff_ID already exists
+        $sql = "SELECT * FROM `staffaccounts` WHERE `staff_ID` = '$staff_id'";
         $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            echo "Error: Username already exists.";
+            echo "<script>
+                    window.location.href = 'Registration page.html';
+                    alert('Error: Staff ID already exists.');
+                </script>";
+            mysqli_close($conn);
+            exit();
+        }
         
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
 
+        //insert data into database
         $sql = "INSERT INTO `staffaccounts` (`staff_ID`,`password`,`f_name`,`l_name`,`position`,`hire_date`,`email_address`,`phone_number`) VALUES ('$staff_id','$password','$first_name','$last_name','$position','$hired_date','$email','$phone_number')";
-        mysqli_query($conn, $sql);
+        
+        if (mysqli_query($conn, $sql)) {
+            echo "Registration successful!";
+            echo "<script>
+                    window.location.href = 'Login_page.html';
+                    alert('Staff account registered successfully!');
+                </script>";
+        } else {
+            echo "<script>
+                      window.location.href = 'Registration page.html';
+                      alert('An error have occured. Staff account not registered successfully');
+                 </script>";
+        }
 
-        header("Location:Login_page.html");
     }
 ?>
